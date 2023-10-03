4dde7c848f891772f63c378d045e09b9937d458c
Dahmani <js_dahmani@esi.dz>
2023-10-03 11:49:46 +0100

    Ajout de la méthode toString

--- Fraction.java
+++ Fraction.java
@@ -1,3 +1,6 @@
 public class Fraction {
     // Variables d'instance, constructeurs, méthodes, etc. peuvent être ajoutés ici
+    public String toString() {
+        return "Je suis une fraction.";
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bonjour, le programme fonctionne !");
+        // Créer un objet Fraction
+        Fraction fraction = new Fraction();
+
+        // Appeler la méthode toString et afficher le résultat
+        String fractionString = fraction.toString();
+        System.out.println(fractionString);
     }
 }
